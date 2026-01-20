d7997ff1d9e8ba6b965bd7e4d90b31a115133c0f
Antony Jose Paul <your-email@example.com>
2026-01-20 22:41:49 +0530

    add pawn movement validation

--- board.py
+++ board.py
@@ -40,11 +40,43 @@ class Board:
         if self.turn == "black" and piece.isupper():
             print("white piece,blacks turn")
             return
+        
+        if piece.lower() == "p":
+            if not self.valid_pawn_move(sr,sc,er,ec,piece):
+                print("invalid pawn move")
+                return
 
 
         self.board[sr][sc] = "."
         self.board[er][ec] = piece
         
-        self.turn += "black" if self.turn == "white" else "white"
+        self.turn = "black" if self.turn == "white" else "white"
+
+
+
+    def valid_pawn_move(self,sr,sc,er,ec,piece):
+        direction = -1 if self.is_white(piece) else 1
+        start_row = 6 if self.is_white(piece) else 1
+
+        if sc == ec and self.board[er][ec]==".":
+            if er == sr + direction :
+                return True
+            if sr == start_row and er == sr + 2 * direction and self.board[sr + direction][sc] == ".":
+                return True
+        if abs(ec-sc) == 1 and er == sr + direction:
+            if self.board[er][ec]!=".":
+                return True
+        return False
+
+
+        
+        
+    def is_white(self,piece):
+        return piece.isupper()
+
+    def is_black(self,piece):
+        return piece.islower()
+
+    
 
 
